7f4286b4d28523cab086c125c65c78c815d7d310
Clarence Tan <clarenciotan@gmail.com>
2017-06-24 17:53:26 +0200

    at chapter "returning results" pp65. working but lacks plugins for maven.

--- src/test/java/features/cash_withdrawal.feature
+++ src/test/java/features/cash_withdrawal.feature
@@ -1,5 +1,5 @@
 Feature: Cash withdrawal
 	Scenario: Successful withdrawal from an accound in credit
-		Given I have $100 in my accound
+		Given I have $17 in my account
 		When I requested $20
 		Then $20 should be dispensed
--- src/test/java/step_definitions/InitialStepDefinition.java
+++ src/test/java/step_definitions/InitialStepDefinition.java
@@ -7,22 +7,30 @@ import cucumber.api.java.en.When;
 
 public class InitialStepDefinition {
 
-	@Given("^I have \\$(\\d+) in my accound$")
-	public void i_have_$_in_my_accound(int arg1) throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	@Given("I have \\$(\\d+) in my account")
+	public void iHave$InMyAccound(int amount) throws Throwable {
+		System.out.println(">>> \"Amount captured: " + amount + "\"");
+		new Account(amount);
 	}
 
 	@When("^I requested \\$(\\d+)$")
-	public void i_requested_$(int arg1) throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	public void iRequested$(int arg1) throws Throwable {
+		throw new PendingException();
 	}
 
 	@Then("^\\$(\\d+) should be dispensed$")
-	public void $_should_be_dispensed(int arg1) throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	public void $ShouldBeDispensed(int arg1) throws Throwable {
+		throw new PendingException();
 	}
-
+	
+	class Account{
+		public Account(int openingBalance)
+		{
+			
+		}
+	}
 }
+
+
+
+
